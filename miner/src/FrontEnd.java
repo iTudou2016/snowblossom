0721f8c85a8018016c13ec38bfe7352b17b3fe53
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2018-07-16 16:19:44 +0800

    Update FrontEnd.java

--- miner/src/FrontEnd.java
+++ miner/src/FrontEnd.java
@@ -19,7 +19,6 @@ import snowblossom.lib.db.rocksdb.JRocksDB;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
-
 import java.math.BigInteger;
 import java.text.DecimalFormat;
 import java.util.concurrent.atomic.AtomicLong;
@@ -28,315 +27,98 @@ import java.util.logging.Logger;
 import java.util.TreeMap;
 import java.util.List;
 
-public class MrPlow
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.TimeZone;
+import duckutil.TaskMaster;
+
+public class FrontEnd
 {
-  private static final Logger logger = Logger.getLogger("snowblossom.miner");
-  
-  public static final int MIN_DIFF=22;
-  public static final int BACK_BLOCKS=5; // Roughly how many blocks back to keep shares for PPLNS
+  private HttpServer server;
 
-  // Basically read this as if there are SHARES_IN_VIEW_FOR_RETARGET
-  // inside a single SHARE_VIEW_WINDOW, then move the miner up one difficulty
-  // So as it is set, if a miner gets 6 shares inside of 2 minutes, move them up.
-  public static final long SHARE_VIEW_WINDOW = 120000L;
-  public static final int SHARES_IN_VIEW_FOR_RETARGET = 6;
-
-  public static void main(String args[]) throws Exception
+  public FrontEnd(Config config)
+		throws Exception
   {
-    Globals.addCryptoProvider();
-    if (args.length != 1)
-    {
-      logger.log(Level.SEVERE, "Incorrect syntax. Syntax: MrPlow <config_file>");
-      System.exit(-1);
-    }
 
-    ConfigFile config = new ConfigFile(args[0]);
+    config.require("port");
+    int port = config.getInt("port");
 
-    LogSetup.setup(config);
-
-    MrPlow miner = new MrPlow(config);
-
-    miner.loop();
+    server = HttpServer.create(new InetSocketAddress(port), 0);
+		server.createContext("/", new GeneralHandler());
   }
 
-  private volatile Block last_block_template;
-
-  private UserServiceStub asyncStub;
-  private UserServiceBlockingStub blockingStub;
-
-  private final NetworkParams params;
-
-  private AtomicLong op_count = new AtomicLong(0L);
-  private long last_stats_time = System.currentTimeMillis();
-  private Config config;
-
-  private TimeRecord time_record;
-  private MiningPoolServiceAgent agent;
-
-  private ShareManager share_manager;
-	private DB db;
-  private ReportManager report_manager;
-
-  public MrPlow(Config config) throws Exception
+  public void start()
+    throws java.io.IOException
   {
-    this.config = config;
-    logger.info(String.format("Starting MrPlow version %s", Globals.VERSION));
-
-    config.require("node_host");
-    config.require("pool_address");
-    config.require("pool_fee");
-    config.require("db_type");
-    config.require("db_path");
-    
-    params = NetworkParams.loadFromConfig(config);
-
-    if (config.getBoolean("display_timerecord"))
-    {
-      time_record = new TimeRecord();
-      TimeRecord.setSharedRecord(time_record);
-    }
-
-
-    int port = config.getIntWithDefault("mining_pool_port",23380);
-    agent = new MiningPoolServiceAgent(this);
-
-    double pool_fee = config.getDouble("pool_fee");
-    double duck_fee = config.getDoubleWithDefault("pay_the_duck", 0.0);
-
-    TreeMap<String, Double> fixed_fee_map = new TreeMap<>();
-    fixed_fee_map.put( AddressUtil.getAddressString(params.getAddressPrefix(), getPoolAddress()), pool_fee );
-    if (duck_fee > 0.0)
-    {
-      fixed_fee_map.put( "snow:crqls8qkumwg353sfgf5kw2lw2snpmhy450nqezr", duck_fee);
-    }
-		loadDB();
-
-		PPLNSState pplns_state = null;
-		try
-		{
-      pplns_state = PPLNSState.parseFrom(db.getSpecialMap().get("pplns_state"));
-      logger.info(String.format("Loaded PPLNS state with %d entries", pplns_state.getShareEntriesCount()));
-		}
-    catch(Throwable t)
-    {
-      logger.log(Level.WARNING, "Unable to load PPLNS state, starting fresh:" + t);
-    }
-
-    share_manager = new ShareManager(fixed_fee_map, pplns_state);
-    report_manager = new ReportManager();
-
-    subscribe();
-
-    Server s = ServerBuilder
-      .forPort(port)
-      .addService(agent)
-      .build();
-
-    s.start();
-  }
-  private void loadDB()
-    throws Exception
-  {
-    String db_type = config.get("db_type");
-
-    if(db_type.equals("rocksdb"))
-    {
-      db = new JRocksDB(config);
-    }
-    else if (db_type.equals("lobstack"))
-    {
-      db = new LobstackDB(config);
-    }
-    else
-    {
-      logger.log(Level.SEVERE, String.format("Unknown db_type: %s", db_type));
-      throw new RuntimeException("Unable to load DB");
-    }
-
-    db.open();
-
+    server.start();
   }
 
-  private void loop()
-    throws Exception
+  public abstract class GeneralHandler implements HttpHandler
   {
-    long last_report = System.currentTimeMillis();
+    @Override
+    public void handle(HttpExchange t) throws IOException {
 
-    while (true)
-    {
-      Thread.sleep(20000);
-      printStats();
-      prune();
-      subscribe();
-      saveState();
-
-      if (config.isSet("report_path"))
+      t.getResponseHeaders().add("Content-Language", "en-US");
+      ByteArrayOutputStream b_out = new ByteArrayOutputStream();
+      PrintStream print_out = new PrintStream(b_out);
+      try
       {
-      if (last_report + 60000L < System.currentTimeMillis())
+        addHeader(print_out);
+        innerHandle(print_out);
+        addFooter(print_out);
+      }
+      catch(Throwable e)
       {
-        report_manager.writeReport(config.get("report_path"));
-
-        last_report = System.currentTimeMillis();
+        print_out.println("Exception: " + e);
       }
-      }
-     
+
+      byte[] data = b_out.toByteArray();
+      t.sendResponseHeaders(200, data.length);
+      OutputStream out = t.getResponseBody();
+      out.write(data);
+      out.close();
 
     }
 
-
-  }
-
-  private void saveState()
-  {
-    PPLNSState state = share_manager.getState();
-    db.getSpecialMap().put("pplns_state", state.toByteString());
-  }
-  private ManagedChannel channel;
-
-  public void recordHashes(long n)
-  {
-    op_count.addAndGet(n);
-  }
-  private void prune()
-  {
-      Block b = last_block_template;
-      if (b!=null)
-      {
-        double diff_delta = PowUtil.getDiffForTarget(BlockchainUtil.targetBytesToBigInteger(b.getHeader().getTarget())) - MIN_DIFF;
-
-        long shares_to_keep = Math.round( Math.pow(2, diff_delta) * BACK_BLOCKS);
-        share_manager.prune(shares_to_keep);
-      }
-  }
-  private void subscribe() throws Exception
-  {
-    if (channel != null)
+    private void addHeader(PrintStream out)
     {
-      channel.shutdownNow();
-      channel = null;
+      out.println("<html>");
+      out.println("<head>");
+      out.println("<title> explorer</title>");
+      out.println("<link rel='stylesheet' type='text/css' href='https://snowblossom.org/style-fixed.css' />");
+      out.println("<link REL='SHORTCUT ICON' href='https://snowblossom.org/snow-icon.png' />");
+      out.println("</head>");
+      out.println("<body>");
+      out.print("<a href='/'>House</a>");
+      out.print("<form name='query' action='/' method='GET'>");
+      out.print("<input type='text' name='search' size='45' value=''>");
+      out.println("<input type='submit' value='Search'>");
+      out.println("<br>");
     }
-
-    String host = config.get("node_host");
-    int port = config.getIntWithDefault("node_port", params.getDefaultPort());
-    channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext(true).build();
-
-    asyncStub = UserServiceGrpc.newStub(channel);
-    blockingStub = UserServiceGrpc.newBlockingStub(channel);
-
-    CoinbaseExtras.Builder extras = CoinbaseExtras.newBuilder();
-    if (config.isSet("remark"))
+    private void innerHandle(PrintStream out)
     {
-      extras.setRemarks(ByteString.copyFrom(config.get("remark").getBytes()));
+      out.println("daskldfjadsj");
+      out.println("fasdasfasfasadsfa");
     }
-    if (config.isSet("vote_yes"))
+    private void addFooter(PrintStream out)
     {
-      List<String> lst = config.getList("vote_yes");
-      for(String s : lst)
-      {
-        extras.addMotionsApproved( Integer.parseInt(s));
-      }
-    }
-    if (config.isSet("vote_no"))
-    {
-      List<String> lst = config.getList("vote_no");
-      for(String s : lst)
-      {
-        extras.addMotionsRejected( Integer.parseInt(s));
-      }
+      out.println("</body>");
+      out.println("</html>");
     }
 
-
-
-    asyncStub.subscribeBlockTemplate(
-      SubscribeBlockTemplateRequest.newBuilder()
-        .putAllPayRatios( share_manager.getPayRatios() )
-        .setExtras(extras.build()).build(),
-                                     new BlockTemplateEater());
-    logger.info("Subscribed to blocks");
-
+    public abstract void innerHandle(HttpExchange t, PrintStream out) throws Exception;
   }
 
-  private AddressSpecHash getPoolAddress() throws Exception
-  {
-      String address = config.get("pool_address");
-      AddressSpecHash to_addr = new AddressSpecHash(address, params);
-      return to_addr;
-  }
-
-  public void stop()
-  {
-    terminate = true;
-  }
-
-  private volatile boolean terminate = false;
-
-  public NetworkParams getParams() {return params;}
-
-  public UserServiceBlockingStub getBlockingStub(){return blockingStub;}
-  public ShareManager getShareManager(){return share_manager;}
-  public ReportManager getReportManager(){return report_manager;}
-
-  public void printStats()
-  {
-    long now = System.currentTimeMillis();
-    double count = op_count.getAndSet(0L);
-
-    double time_ms = now - last_stats_time;
-    double time_sec = time_ms / 1000.0;
-    double rate = count / time_sec;
-
-    DecimalFormat df = new DecimalFormat("0.000");
-
-    String block_time_report = "";
-    if (last_block_template != null)
-    {
-      BigInteger target = BlockchainUtil.targetBytesToBigInteger(last_block_template.getHeader().getTarget());
-
-      double diff = PowUtil.getDiffForTarget(target);
-
-      double block_time_sec = Math.pow(2.0, diff) / rate;
-      double hours = block_time_sec / 3600.0;
-      block_time_report = String.format("- at this rate %s hours per block", df.format(hours));
-    }
-
-    logger.info(String.format("Mining rate: %s", report_manager.getTotalRate().getReportLong(df)));
-
-    logger.info(String.format("Mining rate: %s/sec %s", df.format(rate), block_time_report));
-
-    last_stats_time = now;
-
-    if (config.getBoolean("display_timerecord"))
-    {
-
-      TimeRecord old = time_record;
-
-      time_record = new TimeRecord();
-      TimeRecord.setSharedRecord(time_record);
-
-      old.printReport(System.out);
-
-    }
-  }
-
-  public Block getBlockTemplate()
-  {
-    return last_block_template;
-  }
-
-
-
-  public class BlockTemplateEater implements StreamObserver<Block>
-  {
-    public void onCompleted() {}
-
-    public void onError(Throwable t) {}
-
-    public void onNext(Block b)
-    {
-      logger.info("Got block template: height:" + b.getHeader().getBlockHeight() + " transactions:" + b.getTransactionsCount());
-
-      last_block_template = b;
-      agent.updateBlockTemplate(b);
-    }
-  }
 }
